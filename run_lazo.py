5a9ff12bcd458b5ec981d6f9cdbf75c45668c6db
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-04-20 15:14:03 +0200

    Slight reformatting.

--- run_lazo.py
+++ run_lazo.py
@@ -2,59 +2,58 @@
 import lazo_index_service
 import polars as pl
 import pandas as pd
-from pathlib import Path
 
+from pathlib import Path
+import random
+import string
+
+#%%
+# Connecting to the Lazo server
 SERVER_HOST="localhost"
 SERVER_PORT=15449
 
 lazo_client = lazo_index_service.LazoIndexClient(host=SERVER_HOST, port=SERVER_PORT)
-# %%
-pth = Path("data/yago3-dl/seltab/csv")
-dsname = "yago_seltab_wordnet_organization"
-tgt_dataset = Path(pth, f"{dsname}.csv")
-# %%
-df = pl.read_csv(tgt_dataset)
-columns = df.columns
+
 #%%
-df = df.select(
-    [
-        pl.all().str.lstrip("<").str.rstrip(">"),
-        
-    ]
-)
+# Used only for debugging
+def generate_random_sequence(len_entry, len_sequence):
+    return ["".join(random.choices(string.ascii_letters, k=len_entry)) for _ in range(len_sequence)]
 
 # %%
-df_pd = df.to_pandas()
+pth = Path("data/yago3-dl/seltab/csv")
 # %%
-sketches = lazo_client.get_lazo_sketch_from_data(list("abcd"), dsname, "type")
-# %%
+def read_dataset(tgt_dataset):
+    df = pl.read_csv(tgt_dataset)
+    df = df.select(
+        [
+            pl.all().str.lstrip("<").str.rstrip(">"),
+            
+        ]
+    )
+
+    return df
+
+#%%    
+dsname = "yago_seltab_wordnet_organization"
+tgt_dataset = Path(pth, f"{dsname}.csv")
+df = read_dataset(tgt_dataset)
+
 for col in df.columns:
     (n_permutations, hash_values, cardinality) = lazo_client.index_data(
         df[col].to_list(), dsname, col
     )
-# %%
+#%%
+dsname = "yago_seltab_wordnet_airport"
+tgt_dataset = Path(pth, f"{dsname}.csv")
+df = read_dataset(tgt_dataset)
+
 for col in df.columns:
-    (n_permutations, hash_values, cardinality) = lazo_client.get_lazo_sketch_from_data(
+    (n_permutations, hash_values, cardinality) = lazo_client.index_data(
         df[col].to_list(), dsname, col
     )
 
 # %%
-lazo_sketches = lazo_client.index_data_path(
-    str(tgt_dataset), dsname, columns
-)
-
+# Querying the index
+query = df["subject"].sample(10).to_list()
+lazo_client.query_data(query)
 # %%
-tgt_cols = ['type',
- 'subject',
- 'wasCreatedOnDate',
- 'created',
- 'hasExpenses',
- 'hasRevenue',
- 'hasLongitude',
- 'hasMotto',
- 'participatedIn',
- 'hasBudget',
- 'wasDestroyedOnDate',
- 'hasNumberOfPeople',
- 'hasWonPrize',
- 'isLocatedIn']
